0e5fb058638929a97055cff968a9618d204e63c7
Ryan <68818321+retrobuiltRyan@users.noreply.github.com>
2024-05-13 18:23:34 -0400

    Add files via upload

---
+++ Coin_Pusher_v0_2/Coin_Pusher_v0_2.ino
@@ -0,0 +1,72 @@
+/*Coin Pusher V0_2 by Ryan Bates (c) 2016 Retro Built Games
+Features: Tilt sensor, Tilt LED, speed adjustment for stepper. 
+Basic operation to drive a UNIPOLAR stepper motor with a button and a ULN2803 (8 arrary darlington stepper)
+Code will also work with four TIP120 darlington transistors. If using a Arduino Motor Sheild you'll have to modify this code. 
+
+*/
+const int button3Shot = 11;
+const int buttonSkill = 12;
+const int led = 13;
+const int tiltRelay = 8;
+const int tiltSense = A5;
+long timeout = 86400000; //~1 day in millis
+int speedValue =0;
+int potPin =A0;
+
+long StepperRPM = 100;  // default speed and hold variable
+int buttonState3Shot = HIGH;  
+int buttonStateSkill = HIGH; 
+
+#include <Stepper.h>
+ 
+const int stepsPerRevolution = 200;  //
+Stepper Ystepper(stepsPerRevolution, 2,3,4,5);  
+
+void setup() {
+  Serial.begin(9600);
+   pinMode(button3Shot, INPUT_PULLUP); //enable intermal pull ups. Note all functioning logic must trigger low.
+   pinMode(buttonSkill, INPUT_PULLUP);
+   pinMode(tiltSense, INPUT_PULLUP);
+   pinMode(tiltRelay, OUTPUT);
+   
+  pinMode(led, OUTPUT); 
+  digitalWrite(led,LOW);
+  digitalWrite(tiltRelay,HIGH);
+
+}
+
+void loop() { 
+  speedValue = analogRead(potPin); //read the voltage at the potentiometer ( 0 - 5v)  
+                                    //then convert this reading to the analog resolution number (0-1023)
+                                    
+  if (digitalRead(tiltSense)==LOW) { //if the tilt sensor is not connected, this IF statement will never trigger.
+    digitalWrite(led,HIGH);
+    digitalWrite(tiltRelay, LOW);            // relay triggers on LOW
+    delay(1000); digitalWrite(tiltRelay, HIGH); 
+    delay(1500); //this longer delay is to "wait out" the swinging plum blob. After a hard tilt, 
+                 //the plum bob will continue to swing and may continue to trigger the bell until dampened
+    delay(timeout); // essentially redering the machine out of service [tilt] until the reset button is pressed on the atmega
+  }
+  
+ //----------Stepper Forward-----------------------------------------------------------------------------
+Ystepper.setSpeed(speedValue/10 + 10);      // will give a stepper speed range of ~10 RPM - 110 RPM. Max speed is ~120RPM. if you go beyond 120RPM, motor will skip steps            
+  while ((digitalRead(buttonSkill)==LOW) && (digitalRead(tiltSense)==HIGH)) {     //If button is pressed and tilt has not been activated, drive motor
+    //digitalWrite(led,HIGH);                //when button is pressed (goes low) turn led on and move stepper 200 steps 
+    Ystepper.step(10);                     //200 steps = 1 rotation
+                      }
+//----------Stepper Backward---------------------------------------------------------------------------  
+ Ystepper.setSpeed(speedValue/10 + 10);      //optional to have 2nd button or alternate motor drive profile
+  while (digitalRead(button3Shot) == LOW) { 
+digitalWrite(led,HIGH);    
+    Ystepper.step(-600);                     //-200 steps = 1 rotation (opposite rotation)
+                      }
+                       
+digitalWrite(2,LOW); //disables hold (no current supplied to stepper when idle)
+digitalWrite(3,LOW);
+digitalWrite(4,LOW);
+digitalWrite(5,LOW);
+
+digitalWrite(led,LOW);
+        
+}
+
---
+++ Coin_Pusher_v2.0_with_i2c_LCD/Coin_Pusher_v2.0_with_i2c_LCD.ino
@@ -0,0 +1,108 @@
+/*
+Demonstration sketch for PCF8574T I2C LCD Backpack
+http://tronixlabs.com/display/lcd/serial-i2c-backpack-for-hd44780-compatible-lcd-modules/
+Uses library from https://bitbucket.org/fmalpartida/new-liquidcrystal/downloads
+GNU General Public License, version 3 (GPL-3.0)
+
+If you have the PCF8574T the default I2C bus address is 0x27. 
+If you have the PCF8574AT the default I2C bus address is 0x3F
+*/
+
+
+
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C  lcd(0x3F,2,1,0,4,5,6,7); // 
+
+const int ServoA = 9;
+
+// variables will change:
+const byte interruptPin = 2; //tilt button connection
+byte tiltState=1;
+
+const int clockwise = 1700;
+const int counterclockwise = 1300;
+const int ButtonA = 8;
+const int ledPin =  13; 
+int servoSpeed = 0;
+void setup()
+{
+  // activate LCD module
+  lcd.begin (16,2); // for 16 x 2 LCD module
+  lcd.setBacklightPin(3,POSITIVE);
+  lcd.setBacklight(HIGH);
+
+
+
+ pinMode(interruptPin, INPUT_PULLUP);
+ pinMode(ButtonA, INPUT_PULLUP); 
+ pinMode(ledPin, OUTPUT);
+  // Attach an interrupt to the ISR vector
+  attachInterrupt(digitalPinToInterrupt(interruptPin), tiltActive, CHANGE);
+  
+}
+
+   
+void tiltActive() { // Interrupt Service Routine (ISR)
+  if (digitalRead (interruptPin) == HIGH)
+    digitalWrite (ledPin, HIGH);
+  else
+    digitalWrite (ledPin, LOW);
+}  // end of switchPressed
+
+void loop(){ 
+  lcd.clear(); lcd.home (); // set cursor to 0,0
+  lcd.print("RetroBuiltGames.com"); 
+   if ((digitalRead(ButtonA) == LOW)){ rotate();}
+  delay(1000);
+  scroll();
+
+  lcd.setCursor (0,1);        // go to start of 2nd line
+  //lcd.print(millis());
+  //delay(1000);
+  /*lcd.setBacklight(LOW);      // Backlight off
+  delay(250);
+  lcd.setBacklight(HIGH);     // Backlight on
+  delay(1000);*/
+
+  if (tiltState == LOW){
+  lcd.home(); lcd.clear();
+  for (int blinks = 0; blinks <= 4; blinks ++)
+  { lcd.setBacklight(LOW); delay(50); lcd.setBacklight(HIGH); delay(50);}
+  lcd.print("No Touching!");
+  delay(1000);
+  tiltState = HIGH;
+  }
+}
+
+
+void scroll(){
+  for (int positionCounter = 0; positionCounter < 3; positionCounter++) {
+    // scroll one position left:
+    lcd.scrollDisplayLeft();  
+    delay(250);                                                     }
+delay(2000);   
+  
+ for (int positionCounter = 0; positionCounter < 3; positionCounter++) {
+    // scroll one position right:
+    lcd.scrollDisplayRight();
+    delay(250);   // wait a bit                                                         
+}}
+
+
+void rotate(){  
+  lcd.home(); lcd.clear();
+  lcd.print("Rotating"); lcd.setCursor(0,1);
+  lcd.print("speed=");
+    for(int i=0; i<57; i++) { // change this to adjust +- full revolution
+     digitalWrite(ServoA,HIGH);
+     delayMicroseconds(clockwise); 
+     digitalWrite(ServoA,LOW);
+     delay(18.5); // 18.5ms 
+     //delay(50); enable this line to slow servo rotation
+    }
+
+}
+
+                                                                 
